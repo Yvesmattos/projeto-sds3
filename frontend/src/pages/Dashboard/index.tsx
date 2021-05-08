309b55db1607bbb66f1d90462822ac523d99330d
Yvesmattos <yvesmattos@gmail.com>
2021-05-08 19:54:33 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
-import DonnutChart from "components/DonnutChart";
+import DonutChart from "components/DonnutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -10,22 +10,25 @@ const Dashboard = () => {
             <NavBar />
             <div className="container">
                 <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+                        <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Todas Vendas</h5>
-                        <DonnutChart />
+                        <h5 className="text-center text-secondary">Todas vendas</h5>
+                        <DonutChart />
                     </div>
                 </div>
+
                 <div className="py-3">
                     <h2 className="text-primary">Todas vendas</h2>
                 </div>
+
                 <DataTable />
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 }
